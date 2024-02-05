cdeb96539edd6d97fa30ad369b3f02f4819c7c15
Sivanush <sivanushedapal@gmail.com>
2024-02-06 00:02:19 +0530

    Root changed

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const path = require('path');
 app.set('views',path.join(__dirname,'views'))
 app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname)));
 
 
 app.use(express.json());
